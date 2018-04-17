6d6c032df9ae714240387443b069ef66ced7d1e1
Noy Tsemach <Noy.Tse@walla.co.il>
2018-04-18 00:10:22 +0300

    SecondCommit

--- Client/src/App.js
+++ Client/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
           <TitleAndNavigationBar setSearch={this.setSearch}  showSearchInput={this.state.search}
                                  onKeyUp={e =>{this.setState({searchTxt:e.target.value})}}/>
 
-          <SongList marginTop={this.state.search? "90%" : "75%"} socket={this.socket} search={this.state.searchTxt}/>
+          <SongList marginTop={this.state.search? "90px" : "75px"} socket={this.socket} search={this.state.searchTxt}/>
 
       </div>
     );

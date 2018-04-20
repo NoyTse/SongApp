df2b369e24cf4b1949b9c613fef533bbc4bddb35
Noy Tsemach <Noy.Tse@walla.co.il>
2018-04-20 16:08:18 +0300

    friday

--- Client/src/App.js
+++ Client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
         this.socket = SocketIOClient();
         this.socket.emit('join',"web");
-        window.onfocus(this.socket.emit('join',"web"));
+        window.onFocus(()=> this.socket.emit('join',"web"));
     };
 
     setSearch(val) {
